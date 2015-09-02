8b503bd8abea40ac27487c4ff311c5be4c806c8f
Dafeng Guo <dafeng@strikingly.com>
2015-09-02 21:55:27 +0800

    Site updated: 2015-09-02 21:55:26

--- js/motion_global.js
+++ js/motion_global.js
@@ -64,10 +64,10 @@ $(document).ready(function () {
       { e: $('.logo'), p: { opacity: 1, top: 0 }, o: { duration: 50} }
     ];
 
-    isMist() && sequence.push(
+    /* isMist() && sequence.push(
         { e: $('.logo-line-before i'), p: { translateX: "100%" }, o: { duration: 500, sequenceQueue: false } },
         { e: $('.logo-line-after i'), p: { translateX: "-100%" }, o: { duration: 500, sequenceQueue: false } }
-    );
+    ); */
 
     sequence.push({ e: $('.site-title'), p: { opacity: 1, top: 0 }, o: { duration: 200 } });
 
